435ce672227d71d3629d431ac2cfdbf191613931
Mamadrahimzoda Mehrali <orzuzoda.mehrali@gmai.com>
2025-04-14 17:09:40 +0300

    fsd

--- yatube_api/api/serializers.py
+++ yatube_api/api/serializers.py
@@ -2,7 +2,7 @@ from rest_framework import serializers
 
 from posts.models import Comment, Post, Follow, Group, User
 
-
+#f
 class PostSerializer(serializers.ModelSerializer):
     author = serializers.SlugRelatedField(
         slug_field='username',
